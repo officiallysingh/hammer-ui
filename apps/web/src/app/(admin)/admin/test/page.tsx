808438fbf3c9db99ecdc48e2fb270a26c303a003
parminder singh <parminder@datiot.com>
2026-08-28 11:28:36 +0530

    extra test files removed

--- apps/web/src/app/(admin)/admin/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuctionTimeline from './_components/AuctionTimeline';
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-gray-50 py-12">
-      <AuctionTimeline />
-    </main>
-  );
-}
